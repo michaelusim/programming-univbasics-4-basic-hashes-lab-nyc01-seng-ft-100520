16d98ef0797d9a139833df520319b0b1cc19b88e
michael onyeije <monyeije16@saintpeters.edu>
2020-09-24 18:03:08 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,21 @@
 def new_hash
   # return an empty hash
+   {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  return { :favorite_color => "blue" }
 end
 
 def pioneer
+  return { name: "Grace Hopper" }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  return { id: number }
+  
   # return a hash with a key :id assigned to the provided number
 end
